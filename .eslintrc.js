6a0bd866713e46d62e74d40683c43ce783698652
drzioner <drzioner@gmail.com>
2020-06-12 11:53:16 -0500

    ADD: initial structure

---
+++ .eslintrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  extends: ['plugin:react/recommended'],
+  rules: { quotes: ['error', 'single'] },
+  env: { browser: true, es6: true, jest: true },
+};
---
+++ src/Components/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function App() {
+  return <div>Hola mundo dos</div>;
+}
+
+export default App;
